cef221b15fa4c66862e5422839f2612e95f4d8d5
Philip-Uwaoma <103829082+Philip-Uwaoma@users.noreply.github.com>
2024-12-18 14:01:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ client = AzureOpenAI(
 # Function to fetch and process CSV data
 @st.cache_data
 def load_and_process_data():
-    url = "https://github.com/Philip-Uwaoma/Sn_employee_churn/blob/master/Employee_hcmatrix_data%205.csv"  
+    url = "https://github.com/Philip-Uwaoma/Sn_employee_churn/blob/master/Employee_hcmatrix_data 5.csv"  
     df = pd.read_csv(url,  parse_dates=["Date Of Birth", "Hire Date"])
     # Process the DataFrame
     filtered_df = process_dataframe(df)
