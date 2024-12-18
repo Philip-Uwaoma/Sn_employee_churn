6c8ad170b180cf8e0e86d339030a6df0a6ecaea7
Philip-Uwaoma <103829082+Philip-Uwaoma@users.noreply.github.com>
2024-12-18 13:50:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from matplotlib import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from io import StringIO
 from datetime import datetime
 from openai import AzureOpenAI
