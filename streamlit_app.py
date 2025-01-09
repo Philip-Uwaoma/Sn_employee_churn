7b07a9e7a6911590610ca5e52e7c12fab046bd5e
Philip-Uwaoma <103829082+Philip-Uwaoma@users.noreply.github.com>
2025-01-09 14:22:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -180,7 +180,7 @@ if option == "Individual Prediction":
     st.write(f"Company: {employee_data2['Company Name']} / Department: {employee_data2['Department']} / Status: {employee_data2['Work Status']}")
     st.write(f"### Prediction: {prediction_percentage}%")
     #fig, ax = plt.subplots(figsize=(2, 2))
-    fig, ax = plt.subplots(figsize=(2, 2))  # Larger figure size to scale to center
+    fig, ax = plt.subplots(figsize=(5, 5))  # Larger figure size to scale to center
     #ax.set_position([0.25, 0.25, 0.25, 0.25])  # Position the axes in the center half of the page
     wedges, _ = ax.pie([prediction_percentage, 100 - prediction_percentage], startangle=90, colors=[color, "lightgrey"], wedgeprops=dict(width=0.3))
     # Adjust the plot area to occupy only one-fourth of the page and center it
